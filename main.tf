0d3104372efe4c1ec71cafed8fb2e47a2246c645
austinrain03 <austin.rain@me.com>
2023-04-11 13:44:05 -0500

    Add Terraform code for creating a VPC with public and private subnets

--- main.tf
+++ main.tf
@@ -1,47 +1,113 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.60.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.1.1.0/24"
 
-resource "aws_vpc" "main" {
-  cidr_block                       = "10.0.0.0/16"
-  assign_generated_ipv6_cidr_block = true
-  enable_dns_hostnames             = true
-  enable_dns_support               = true
   tags = {
-    "Name" = "${var.default_tags.env}-VPC"
+    Name = "my-vpc"
   }
 }
 
+# Create public subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.1.1.0/26"
+  availability_zone = "us-east-1a"
 
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  map_public_ip_on_launch = true
   tags = {
-    "Name" = "${var.default_tags.env}-Public-Subnet"
+    Name = "public-subnet-1"
   }
-  availability_zone = data.aws_availability_zones.availabilty_zone.names[count.index]
 }
 
-resource "aws_subnet" "private" {
-  count      = 2
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + var.public_subnet_count)
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.1.1.64/26"
+  availability_zone = "us-east-1b"
+
   tags = {
-    "Name" = "${var.default_tags.env}-Private-Subnet"
+    Name = "public-subnet-2"
   }
-  availability_zone = data.aws_availability_zones.availabilty_zone.names[count.index]
 }
 
-# this is a test 
+# Create private subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.1.1.128/26"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.1.1.192/26"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "my-nat-gateway"
+  }
+}
+
+# Create EIP
+resource "aws_eip" "my_eip" {
+  vpc = true
+
+  tags = {
+    Name = "my-eip"
+  }
+}
+
+# Create public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Create private route table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Create routes in public route table
+resource "aws_route" "public_internet_gateway_route" {
+  route_table_id = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my_igw.id
+}
+
+# Create routes in private route table
+resource "aws_route" "private_nat_gateway_route" {
+  route_table_id = aws_route_table.private_route_table.id
+  destination_cidr_block = "10.1.0.0/16"
+}
